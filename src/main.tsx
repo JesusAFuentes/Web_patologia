e4ca09c657da4a5049e3c3df9a21af9bbb012800
AlejandroF <125092602+JesusAFuentes@users.noreply.github.com>
2024-06-10 08:49:54 -0700

    adding path of clinical studies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './App';
+import ClinicalStudies from './ClinicalStudies';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Login/>} />
+      <Route path="/Clinical_Studies" element={<ClinicalStudies />} />
+    </Routes>
+  </Router>,
+  document.getElementById('root')
+);
