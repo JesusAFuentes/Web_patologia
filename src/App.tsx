d0e4deed1d66c4c2801301aab4392980cc34812d
AlejandroF <125092602+JesusAFuentes@users.noreply.github.com>
2024-06-10 08:50:21 -0700

    basic login

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+import microscopio from './images/microscopio.png'
+function Login() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="logo">
+          <img src={microscopio} alt="Microscopio" className="microscopio" />
+          <label className="patologia">Patologia Guillermo Patiño</label>
+        </div>
+        <div className="div-login">
+          <div className="input-group">
+            <label htmlFor="email">Username</label>
+            <input id="email" placeholder="Username" required type="email" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder="********" name="password" id="password" />
+          </div>
+          <div className="inicio">
+            <button>Iniciar Sesion</button>
+            <a href="#">Forgot password?</a>
+
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default Login
